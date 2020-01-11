0a8b753068f37f1dc31c59de3ea2d0a9d8489985
Maksim Vinokur <maksimus.vinokur@gmail.com>
2020-01-11 17:07:59 +0200

    authfrom start

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import {
     BrowserRouter as Router,
     Switch,
@@ -8,25 +8,42 @@ import {
   }  from 'react-router-dom'
 import NavBar from 'Components/navbar'
 import {signup, login, isAuthorized} from "Utils/auth"
+const LoginForm = React.lazy(() => import("Components/AuthForms/LoginForm"))
+const SignupForm = React.lazy(() => import("Components/AuthForms/SignupForm"))
+import './App.scss'
 export default function App() {
 return (<Router>
-    
-   <NavBar/>
+  <NavBar>
+    <Link to="" className="nav-link">Home</Link>
+    <Link to="" className="nav-link">Home</Link>
+    <Link to="" className="nav-link">Home</Link>
+  </NavBar>
+   
     <Switch>
         <Route path="/">
           { 
           !isAuthorized() ? (
             <></>
           ) : (
-            <>
-              
-            </>
-          )
+              <Redirect to="/login"/>          
+              )
     
           }
-        
-          
         </Route>
+        <Suspense>
+        <Route path="/login">
+            <div className="display-center"> 
+            </div>
+        </Route>
+        <Route path="/signup">
+
+        </Route>
+        </Suspense>
     </Switch>
 </Router>) 
 }
+
+
+
+
+
--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -2,24 +2,24 @@ import React, {useState} from "react";
 import {Link} from 'react-router-dom'
 
 export default function NavBar(props) {
-    const [items, setItems] = useState([1,0,0])
+    const [items, setItems] = useState([1].concat(new Array(props.children.length-1).fill(0)))
     const Activate = (n) => {
-      setItems(items.map((v,i) => i==n?true:false) )
+      setItems(items.map((v,i) => i==n ? true:false) )
     }
     return (
     <div className="navbar navbar-expand-lg bg-light navbar-light">
     <Link to="#" className="navbar-brand font-weight-light">Worder</Link>
     <div className="collapse navbar-collapse ml-4" id="navbarContent">
       <ul className="navbar-nav mr-auto">
-        <Item isActive={() => items[0]} onClick={() => {Activate(0)}}> 
-          <Link className="nav-link" to="">Home</Link>
-        </Item>
-        <Item isActive={() => items[1]} onClick={() => {Activate(1)}}>  
-          <Link className="nav-link" to="">example</Link>
-        </Item>
-        <Item isActive={() => items[2]}  onClick={() => {Activate(2)}}>
-          <Link className="nav-link" to="">example</Link>
-        </Item>
+        {
+          props.children.map((v,i) => {
+            return (
+            <Item key={i} isActive={() => items[i]} onClick={() => {Activate(i)}}>
+              {v}
+            </Item>
+            )
+          })
+        }
       </ul>
     </div>
   </div>)
