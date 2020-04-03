e5f0cc68e1f356cbfc15a901c47fbf17b2e0154a
Maksim Vinokur <maksimus.vinokur@gmail.com>
2020-04-03 18:30:01 +0300

    dict routes

--- src/components/dicts/index.js
+++ src/components/dicts/index.js
@@ -1,8 +1,35 @@
 import React, { Suspense, useState } from "react";
-import {Route, Switch, Link } from "react-router-dom";
-
+import { Route, Switch, Link, useRouteMatch } from "react-router-dom";
+import * as api from 'Api/dictionary'
+const Play = React.lazy(() => import("./play"));
+const Details = React.lazy(() => import("./details"));
+const List = React.lazy(() => import('./list'))
 export default function Dictionaries(props) {
-    return (<div className="dictionary-container">
-        DICTS
-    </div>)
+  const { path, url } = useRouteMatch();
+  return (
+    <div className="dictionary-container">
+      <Switch>
+        <Route path={[`${path}/`]}>
+        <Suspense fallback={<></>}>
+          <List generator={dicts}> </List>
+          </Suspense>
+        </Route>
+        <Route path={[`${path}/details/:dictId`]}>
+          <Suspense fallback={<></>}>
+            <Details></Details>
+          </Suspense>
+        </Route>
+        <Route path={[`${path}/play/:dictId`]}>
+        <Suspense fallback={<></>}>
+          <Play></Play>
+        </Suspense>
+        </Route>
+      </Switch>
+    </div>
+  );
 }
+ function* dicts() {
+   for(id in api.dicts()) {
+      yield api.detailsById(id)
+   }
+ }
