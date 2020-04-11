346fd528d5a5f6f7b233368dec250740da8d7027
Maksim Vinokur <maksimus.vinokur@gmail.com>
2020-04-12 01:25:44 +0300

    navbar refactor

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,13 @@
-import React, { useState, Suspense } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import React, { Suspense } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import NavBar from "Components/navbar";
 const Dicts = React.lazy( () => import('Components/dicts'))
 import "./App.scss";
 
 export default function App() {
   return (
     <Router>
-      <NavBar>
-        <Link to="" className="nav-link">
-          Home
-        </Link>
-        <Link to="" className="nav-link">
-          Home
-        </Link>
-        <Link to="" className="nav-link">
-          Home
-        </Link>
-      </NavBar>
+
       <Switch>
         <Route path='/'>
         <div className="app-grid container-fluid">
