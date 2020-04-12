90f4763133a27fe52afcb16400f63945dcbe0bef
Maksim Vinokur <maksimus.vinokur@gmail.com>
2020-04-12 16:19:41 +0300

    route refactor

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,27 @@
 import React, { Suspense } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
-const Dicts = React.lazy( () => import('Components/dicts'))
+const Dicts = React.lazy(() => import("Components/dicts"));
 import "./App.scss";
 
 export default function App() {
   return (
     <Router>
-
       <Switch>
-        <Route path='/'>
-        <div className="app-grid container-fluid">
-          <Switch>
-            <Route path={['/', '/dicts/']}>
-              <Suspense fallback={<></>}>
-                <Dicts></Dicts>
-              </Suspense>
-              
-            </Route>
-          </Switch>
-        </div>
+        <Route path={["/dicts"]}>
+          <Suspense fallback={<></>}>
+            <Dicts></Dicts>
+          </Suspense>
         </Route>
-        
+        <Route path={["/"]}>
+          <Redirect to="/dicts" />
+        </Route>
       </Switch>
-      
-
     </Router>
   );
 }
